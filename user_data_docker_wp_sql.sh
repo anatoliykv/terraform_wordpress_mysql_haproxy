e2af1e835ce5293bac4cfde5fe03fd812fd1d335
Anatoliy <anatoliykv@gmail.com>
2020-01-14 22:10:14 +0200

    Update user_data_docker_wp_sql.sh

--- user_data_docker_wp_sql.sh
+++ user_data_docker_wp_sql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo apt upgrade -y
+#sudo apt upgrade -y
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
